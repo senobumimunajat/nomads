264390354b8a3b23090c2cd1dfa50637a6cffaf0
senobumimunajat <senobumimunajat15@gmail.com>
2022-04-17 23:39:00 +0700

    auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -47,6 +47,12 @@
                                     <label class="form-check-label" for="remember">
                                         {{ __('Remember Me') }}
                                     </label>
+                                    
+                                @if (Route::has('password.request'))
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
+                            @endif
                                 </div>
                             </div>
                         </div>
@@ -56,12 +62,13 @@
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                
+                                @if (Route::has('register'))
+                                    <a class="btn btn-link" href="{{ route('register') }}">
+                                        {{ __('Register Here') }}
                                     </a>
                                 @endif
+
                             </div>
                         </div>
                     </form>
