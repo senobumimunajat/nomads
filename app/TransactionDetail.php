f2a6061ec6f59cbc0312845d78ce0df1d1e480ba
senobumimunajat <senobumimunajat15@gmail.com>
2022-04-23 23:57:02 +0700

    transaction CRUD

---
+++ app/TransactionDetail.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+class TransactionDetail extends Model
+{
+    use SoftDeletes;
+    
+    protected $fillable = [
+        'transaction_id', 'username', 'nationalty', 'is_visa', 'doe_passport'
+
+    ];
+
+    protected $hidden = [
+    ];
+
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class, 'transactions_id', 'id');
+    }
+
+ 
+}
